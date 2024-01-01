ec622fac4d8c586032138e49414415466061559d
M7Khouli <mohammed.alkhouli771@gmail.com>
2024-01-02 01:03:30 +0300

    add employees

--- prisma/migrations/20240101215219_init/migration.sql
+++ prisma/migrations/20240101215219_init/migration.sql
@@ -32,6 +32,16 @@ CREATE TABLE `Customer` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
+CREATE TABLE `Employee` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `address` VARCHAR(191) NOT NULL,
+    `phoneNumber` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `Sale` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `quantity` INTEGER NOT NULL,
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const dotEnv = require("dotenv");
 
-dotEnv.config({ path: "./config.env" });
+dotEnv.config({ path: "./.env" });
 
 process.on("uncaughtException", (err) => {
   console.log("Unhandled Exception!!!💥💥 shutting down....");
